c3183bf3a3b5ba41e1ae4ce128c23efd29cdd3ee
ahmad.qasim <ahmad.live.1@gmail.com>
2019-09-22 12:39:47 +0200

    implementing a GAN network which uses the MNIST dataset

---
+++ gan.py
@@ -0,0 +1,122 @@
+import numpy as np
+import fire
+import cv2
+import matplotlib.pyplot as plt
+from keras.layers import Input, Dense, BatchNormalization
+from keras.models import Model, load_model, save_model
+from keras.datasets import mnist
+
+
+class GAN:
+    def __init__(self):
+        self.discriminator = None
+        self.generator = None
+        self.gan = None
+        self.gan_input = (100, )
+        self.data_shape = (28, 28)
+        self.generator_input = self.gan_input
+        self.generator_output = (np.prod(self.data_shape), )
+        self.X_train = None
+        self.X_test = None
+        self.batch_size = 32
+        self.epochs = 100
+        self.generator_model_path = 'models/gan.hdf5'
+        self.test_count = 9
+
+    def create_discriminator(self):
+        input = Input(self.generator_output)
+
+        x = Dense(256, activation='relu')(input)
+        x = Dense(64, activation='relu')(x)
+        x = BatchNormalization()(x)
+
+        output = Dense(1, activation='sigmoid')(x)
+
+        self.discriminator = Model(input=input, output=output)
+        self.discriminator.compile(loss='binary_crossentropy', optimizer='adam')
+
+    def create_generator(self):
+        input = Input(self.gan_input)
+
+        x = Dense(256, activation='relu')(input)
+        x = Dense(512, activation='relu')(x)
+        x = BatchNormalization()(x)
+
+        output = Dense(self.generator_output[0], activation='sigmoid')(x)
+
+        self.generator = Model(input=input, output=output)
+        self.generator.compile(loss='binary_crossentropy', optimizer='adam')
+
+    def create_gan(self):
+        self.discriminator.trainable = False
+
+        input = Input(self.gan_input)
+
+        x = self.generator(input)
+        output = self.discriminator(x)
+
+        self.gan = Model(input=input, output=output)
+        self.gan.compile(loss='binary_crossentropy', optimizer='adam')
+
+    def load_data(self):
+        (self.X_train, _), (self.X_test, _) = mnist.load_data()
+
+    def train(self):
+        self.load_data()
+        self.create_discriminator()
+        self.create_generator()
+        self.create_gan()
+
+        for i in range(self.epochs):
+            for k in range(int(self.X_train.shape[0]/self.batch_size)):
+                noise = np.random.normal(0, 1, (self.batch_size, 100))
+                minibatch_x = self.X_train[k*self.batch_size:(k+1)*self.batch_size]
+                minibatch_x = np.reshape(minibatch_x, (self.batch_size, 784))
+                minibatch_x = minibatch_x / 255
+                minibatch_y = np.ones(self.batch_size)
+                generated_x = self.generator.predict(noise)
+                generated_y = np.zeros(self.batch_size)
+
+                self.discriminator.trainable = True
+                self.discriminator.fit(minibatch_x, minibatch_y, batch_size=self.batch_size, verbose=0)
+                self.discriminator.fit(generated_x, generated_y, batch_size=self.batch_size, verbose=0)
+
+            noise = np.random.normal(0, 1, (self.batch_size, 100, ))
+            gan_y = np.ones(self.batch_size)
+
+            self.gan.fit(noise, gan_y, batch_size=self.batch_size, verbose=0)
+
+            if i % 5 == 0:
+                self.sample_gan(i)
+                save_model(self.generator, self.generator_model_path)
+
+            print("Epoch: ", i+1)
+
+    def sample_gan(self, epoch):
+        noise = np.random.normal(0, 1, (1, 100))
+        img = self.generator.predict(noise)
+        img = np.reshape(img, (28, 28))
+        img = img * 255
+        cv2.imwrite('gan_generated/img_{}.png'.format(epoch), img)
+
+    def plot_results(self, generated):
+        fig = plt.figure(figsize=(28, 28))
+        columns = np.sqrt(self.test_count)
+        rows = np.sqrt(self.test_count)
+        for i in range(1, columns * rows + 1):
+            fig.add_subplot(rows, columns, i)
+            plt.imshow(generated[i])
+        plt.show()
+
+    def test(self):
+        generator = load_model(self.generator_model_path)
+        generated = []
+        for _ in range(self.test_count):
+            noise = np.random.normal(0, 1, (1, 100))
+            generated.append(generator.predict(noise))
+
+        self.plot_results(generated)
+
+
+if __name__ == "__main__":
+    fire.Fire(GAN)
