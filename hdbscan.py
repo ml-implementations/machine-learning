f374b85e04cc462fbf2ca65f8fc5224a85f5cd91
ahmad.qasim <ahmad.live.1@gmail.com>
2020-05-11 20:31:30 +0200

    adding the first commit for the prim's algorithm

--- hdbscan.py
+++ hdbscan.py
@@ -24,7 +24,7 @@ class Utils:
         plot_kwds = {'alpha': 0.5, 's': 80, 'linewidths': 0}
 
         plt.scatter(test_data.T[0], test_data.T[1], color=color, **plot_kwds)
-        plt.show()
+        # plt.show()
 
     @staticmethod
     def get_dist(test_data):
@@ -52,10 +52,31 @@ class Utils:
 
         return dist_mat
 
+    @staticmethod
+    def prims_algorithm(dist_mat):
+        not_visited = list(range(1, dist_mat.shape[0]))
+        edges = np.zeros(shape=(dist_mat.shape[0], dist_mat.shape[0]))
+
+        visited = [0]
+
+        while len(not_visited):
+            vertex = np.argmin(dist_mat[visited, not_visited])
+            q, r = divmod(int(vertex), len(visited))
+            if len(visited) == 1:
+                r, q = q, r
+            q, r = visited[q], not_visited[r]
+            edges[q, r] = 1
+            visited.append(r)
+            not_visited.remove(r)
+
+        return edges
+
 
 if __name__ == '__main__':
     data = Utils.get_data()
     Utils.plot_data(data)
     dist = Utils.get_dist(data)
     core_dist = Utils.core_dist(dist)
-    dist = Utils.transform_space(dist, core_dist)
+    transformed_dist = Utils.transform_space(dist, core_dist)
+    e = Utils.prims_algorithm(transformed_dist)
+    pass
