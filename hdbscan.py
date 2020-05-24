9effb27e9d9a2056829da54127c330bc3f66f9dc
ahmad.qasim <ahmad.live.1@gmail.com>
2020-05-24 17:34:14 +0200

    adding the condensed clusters method

--- hdbscan.py
+++ hdbscan.py
@@ -18,7 +18,7 @@ class Utils:
         return test_data
 
     @staticmethod
-    def plot_data(test_data, edges, transformed_dist, color='b'):
+    def plot_data(test_data, edges, transformed_distance, color='b'):
         sns.set_context('poster')
         sns.set_style('white')
         sns.set_color_codes()
@@ -30,7 +30,7 @@ class Utils:
             for j in range(test_data.shape[0]):
                 if edges[i, j]:
                     plt.plot([test_data[i, 0], test_data[j, 0]], [test_data[i, 1], test_data[j, 1]], 'k-',
-                             linewidth=transformed_dist[i, j])
+                             linewidth=transformed_distance[i, j])
         plt.show()
 
         return True
@@ -48,8 +48,8 @@ class Utils:
     def core_dist(dist_mat, k=5):
         core_mat = np.zeros(shape=dist_mat.shape[0])
         for i in range(dist_mat.shape[0]):
-            dist = np.sort(dist_mat[i, :])
-            core_mat[i] = dist[k-1]
+            distance = np.sort(dist_mat[i, :])
+            core_mat[i] = distance[k-1]
 
         return core_mat
 
@@ -80,31 +80,62 @@ class Utils:
         return edges
 
     @staticmethod
-    def cluster_hierarchy(e, num_points):
-        Z = np.zeros(shape=(num_points-1, 4))
-        e_sorted = np.argsort(e)[-(num_points-1):]
+    def cluster_hierarchy(edges, num_points):
+        out = np.zeros(shape=(num_points-1, 4))
+        e_sorted = np.argsort(edges.flatten())[-(num_points - 1):]
         hierarchy_nodes = {i: [i, 1] for i in range(num_points)}
         hierarchy_clusters = {i: [i] for i in range(num_points)}
+        hierarchy_tree = dict()
         curr_hierarchy = num_points - 1
         for i in range(e_sorted.shape[0]):
             q, r = divmod(e_sorted[i], num_points)
-            Z[i, 0], Z[i, 1] = hierarchy_nodes[q][0], hierarchy_nodes[r][0]
-            Z[i, 2] = e[e_sorted[i]]
-            Z[i, 3] = hierarchy_nodes[q][1] + hierarchy_nodes[r][1]
+            s, t = hierarchy_nodes[q][1], hierarchy_nodes[r][1]
+            out[i, 0], out[i, 1] = hierarchy_nodes[q][0], hierarchy_nodes[r][0]
+            out[i, 2] = np.amax(edges[hierarchy_clusters[out[i, 0]], :][:, hierarchy_clusters[out[i, 1]]])
+            out[i, 3] = s + t
             curr_hierarchy += 1
-            for j in hierarchy_clusters[Z[i, 0]]:
-                hierarchy_nodes[j] = [curr_hierarchy, Z[i, 3]]
-            for k in hierarchy_clusters[Z[i, 1]]:
-                hierarchy_nodes[k] = [curr_hierarchy, Z[i, 3]]
-            hierarchy_clusters.update({curr_hierarchy: hierarchy_clusters[Z[i, 0]] + hierarchy_clusters[Z[i, 1]]})
-        return Z
+            for j in hierarchy_clusters[out[i, 0]]:
+                hierarchy_nodes[j] = [curr_hierarchy, out[i, 3]]
+            for k in hierarchy_clusters[out[i, 1]]:
+                hierarchy_nodes[k] = [curr_hierarchy, out[i, 3]]
+            hierarchy_clusters.update({curr_hierarchy: hierarchy_clusters[out[i, 0]] + hierarchy_clusters[out[i, 1]]})
+            hierarchy_tree.update({curr_hierarchy: [[out[i, 0], s], [out[i, 1], t], out[i, 2]]})
+        return out, hierarchy_tree, hierarchy_clusters
 
     @staticmethod
-    def plot_dendrogram(Z):
-        dn = hierarchy.dendrogram(Z)
+    def plot_dendrogram(out):
+        dendrogram = hierarchy.dendrogram(out)
         plt.show()
 
-        return dn
+        return dendrogram
+
+    @staticmethod
+    def condense_cluster_tree(hierarchy_tree, hierarchy_clusters, min_cluster_size):
+        points_lambda = dict()
+        clusters_lambda = dict()
+
+        for k, v in hierarchy_tree.items():
+            if v[0][1] < min_cluster_size:
+                for i in hierarchy_clusters[v[0][0]]:
+                    points_lambda.update({i: 1/v[2]})
+            else:
+                clusters_lambda.update({v[0][0]: 1/v[2]})
+            if v[1][1] < min_cluster_size:
+                for j in hierarchy_clusters[v[1][0]]:
+                    points_lambda.update({j: 1/v[2]})
+            else:
+                clusters_lambda.update({v[1][0]: 1/v[2]})
+
+        return points_lambda, clusters_lambda
+
+    @staticmethod
+    def extract_clusters(hierarchy_clusters, points_lambda, clusters_lambda):
+        clusters_stabilities = dict()
+
+        for k, v in hierarchy_clusters.items()[:-1]:
+            pass
+
+        return True
 
 
 if __name__ == '__main__':
@@ -114,6 +145,7 @@ if __name__ == '__main__':
     transformed_dist = Utils.transform_space(dist, core_dist)
     e = Utils.prims_algorithm(transformed_dist)
     plot = Utils.plot_data(data, e, transformed_dist)
-    Z = Utils.cluster_hierarchy(e.flatten(), num_points=transformed_dist.shape[0])
+    Z, tree, clusters = Utils.cluster_hierarchy(e, num_points=transformed_dist.shape[0])
     dn = Utils.plot_dendrogram(Z)
-    pass
+    p_lambda, c_lambda = Utils.condense_cluster_tree(tree, clusters, min_cluster_size=5)
+    clusters = Utils.extract_clusters(clusters, p_lambda, c_lambda)
